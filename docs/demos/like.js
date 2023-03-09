d392d3120c8890798d7eaa247c3d9aa3234eed5e
theia <theia@mit.edu>
2023-03-09 11:34:28 -0500

    docs factored out

--- docs/demos/like.js
+++
@@ -1,9 +0,0 @@
-
-export default function({useCollection}) { return {
-
-  props: ['word'],
-  setup: (props)=> useCollection(()=> ({
-    defines: props.word,
-    definition: { $type: 'string' },
-    timestamp: { $type: 'number' }
-  })),
